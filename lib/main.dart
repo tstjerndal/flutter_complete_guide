5f6417a8ab581d1db482a542cffe3871675c6cd9
Tommy Stjerndal <t.stjerndal@gmail.com>
2019-08-30 15:32:56 +0200

    commit 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,30 @@
 import 'package:flutter/material.dart';
 import './question.dart';
+import './answer.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget{
-@override
+class MyApp extends StatefulWidget {
+  @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return _MyAppState() ;
+    return _MyAppState();
   }
 }
 
-class _MyAppState extends State <MyApp>{
+class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
-  void _answerQuestion(){
+  void _answerQuestion() {
     setState(() {
-      _questionIndex = _questionIndex+1;
+      _questionIndex = _questionIndex + 1;
     });
 
-    print (_questionIndex);
+    print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions= [
+    var questions = [
       'Whats your favorite color',
       'Whats your favorite animal',
     ];
@@ -37,19 +38,11 @@ class _MyAppState extends State <MyApp>{
         body: Column(
           children: [
             Question(
-            questions[_questionIndex]),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: _answerQuestion,
+              questions[_questionIndex],
             ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: _answerQuestion,
-            ),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
           ],
         ),
       ),
