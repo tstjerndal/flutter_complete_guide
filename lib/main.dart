1734243240290dcd034798a1c02f3030816018b3
Tommy Stjerndal <t.stjerndal@gmail.com>
2019-08-30 12:41:23 +0200

    commit 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget{
+@override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState() ;
+  }
+}
+
+class _MyAppState extends State <MyApp>{
+  var _questionIndex = 0;
+  void _answerQuestion(){
+    setState(() {
+      _questionIndex = _questionIndex+1;
+    });
+
+    print (_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions= [
+      'Whats your favorite color',
+      'Whats your favorite animal',
+    ];
 
-    void answerQuestion(){
-      print ('Answer choosen!');
-    }
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
@@ -17,18 +36,19 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The question'),
+            Question(
+            questions[_questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
           ],
         ),
