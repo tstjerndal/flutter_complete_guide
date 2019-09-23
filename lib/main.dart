91fb83e381df7e8bc134215fc0b0ca2041b8d9e5
Tommy Stjerndal <t.stjerndal@gmail.com>
2019-09-23 08:22:52 +0200

    lession 50

--- lib/main.dart
+++ lib/main.dart
@@ -45,6 +45,13 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   var _totalScore = 0 ;
 
+  void _resetQuiz(){
+    setState(() {
+    _questionIndex = 0;
+    _totalScore = 0;
+    });
+  }
+
   void _answerQuestion(int score) {
     if (_questionIndex < _questions.length) {
       print('We hav more questions');
@@ -72,7 +79,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(_totalScore),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final Function resetHandler;
   Result(this.resultScore);
 
-  String get resultPhrase{
-
+  String get resultPhrase {
     var resultText = 'You did it';
 
-    if (resultScore <= 8){
+    if (resultScore <= 8) {
       resultText = 'you are awesome and innocent';
-    } else if (resultScore <= 12){
+    } else if (resultScore <= 12) {
       resultText = 'Pretty likeable!';
-    } else if (resultScore <= 16){
+    } else if (resultScore <= 16) {
       resultText = 'You are strange!';
     } else {
       resultText = 'You are so bad!';
@@ -23,10 +23,15 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(child: Text('Restart Quiz'),textColor: Colors.blue,onPressed: resetHandler,),
+        ],
       ),
     );
   }
